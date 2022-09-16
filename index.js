303cf7f590762c4a1710a3fb53a12735c50d24e3
Annie <annie_baines@hotmail.co.uk>
2022-09-16 16:58:18 +0100

    add startGame function and put conditional within

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 let firstCard = 10
 let secondCard = 11
-let sum = firstCard + secondCard
+let sum = firstCard + secondCard 
 let hasBlackJack = false
+let isAlive = true
+let message = ""
 
-if (sum <= 20) {
-    console.log("Do you want to draw a new card? :-)")
-} else if (sum === 21) {
-    console.log("Wohoo! You've got Blackjack! :-D")
-    hasBlackJack = true
-} else {
-    console.log("You're out of the game! :-(")
+
+
+
+function startGame() {
+    if (sum <= 20) {
+        message = "Do you want to draw a new card? 🙂"
+    } else if (sum === 21) {
+        message = "Wohoo! You've got Blackjack! 🥳"
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game! 😭"
+        isAlive = false
+    }
+
+    console.log(message)
 }
-
-    console.log(hasBlackJack)
