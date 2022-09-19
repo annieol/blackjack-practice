e44769c8125272141804cb9578e73456d098de0b
Annie <annie_baines@hotmail.co.uk>
2022-09-19 16:08:48 +0100

    Deal random cards only after start game button is clicked. Will not deal new cards if number is >20

--- index.js
+++ index.js
@@ -1,17 +1,43 @@
-let firstCard = 10
-let secondCard = 4
-let cards = [firstCard, secondCard]
-let sum = firstCard + secondCard 
+let cards = []
+let sum = 0
 let hasBlackJack = false
-let isAlive = true
+let isAlive = false
 let message = ""
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.querySelector("#cards-el")
 
+console.log(cards)
+
+function startGame() {
+    isAlive = true
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard 
+    renderGame()
+}
+
+function getRandomCard() {
+    let randomCard = Math.floor( Math.random()*13 ) +1
+    if (randomCard > 10) {
+        return 10
+    }
+    else if (randomCard === 1) {
+        return 11
+    }
+    else {
+        return randomCard
+    }
+}
+
+
 
 function renderGame() {
-    cardsEl.textContent = "Cards: " + cards[0] + ", " + cards[1]
+    cardsEl.textContent = "Cards: " 
+    for (let i = 0; i < cards.length; i++) {
+        cardsEl.textContent += cards[i] + " "
+    }
     sumEl.textContent = "Sum: " + sum
     if (sum <= 20) {
         message = "Do you want to draw a new card?"
@@ -27,16 +53,14 @@ function renderGame() {
 }
 
 function newCard() {
+    if (isAlive === true && hasBlackJack === false) {
     console.log("Drawing a new card from the deck!")
-    let card = 6
+    let card = getRandomCard()
     sum += card
     cards.push(card)
     renderGame()
-    cardsEl.textContent = "Cards: " + cards[0] + ", " + cards[1] + ", " + cards[2]
-    
+    }
 }
 
-function startGame() {
-    renderGame()
-}
+
 
